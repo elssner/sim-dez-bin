91f196b8f416f3597c6c06122db70a7784440dc3
GTA Calliope <73078489+elssner@users.noreply.github.com>
2024-01-03 22:05:49 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    a = BitArray(31)
-    bit.plot25LED(1, a)
+    a = BitArray(22)
+    bit.plot25LEDn(0, a)
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     for (let Index = 0; Index <= 2 ** 6 - 1; Index++) {
-        bit.plot25LED(3, BitArray2(Index))
+        bit.plot25LEDn(3, BitArray(Index))
         basic.pause(500)
     }
 })
@@ -13,26 +13,15 @@ function BitArray (dezParameter: number) {
     d = Math.trunc(dezParameter)
     while (d > 0) {
         rest = d % 2
-        boolean = rest == 1
-        list.push(boolean)
+        list.push(rest)
         d = Math.trunc(d / 2)
     }
     return list
 }
-function BitArray2 (dezParameter: number) {
-    list = []
-    d = Math.trunc(dezParameter)
-    while (d > 0) {
-        list.push(d % 2 == 1)
-        d = bit.bitwise(d, bit.eBit.RIGHT, 1)
-    }
-    return list
-}
-let boolean = false
 let rest = 0
 let d = 0
-let list: boolean[] = []
-let a: boolean[] = []
+let list: number[] = []
+let a: number[] = []
 bit.comment("elssner/sim-dez-bin")
 bit.comment("Erweiterung laden:")
 bit.comment("calliope-net/bit")
